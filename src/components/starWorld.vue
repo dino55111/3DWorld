7e7cad4b7c25bbec202ec86b795064c00d1784d2
Dino.chang <dino55111@gmail.com>
2023-10-08 18:43:34 +0800

    [功能] 撰寫 3D 星空效果

--- src/components/starWorld.vue
+++ src/components/starWorld.vue
@@ -9,29 +9,17 @@
 </template>
 
 <script setup>
-import * as THREE from 'three'
 import { initial } from '@/plugin/starWorld.js'
 import { ref } from 'vue'
 
 const starWorld = ref(null)
-let scene = null
-let camera = null
   
 // animate()
 
 onMounted(() => {
   const threeObj = initial(starWorld.value)
-  scene = threeObj.scene
-  camera = threeObj.camera
 
   threeObj.animate()
-  
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
-  
-  camera.position.z = 5
 })
 </script>
 
--- src/plugin/starWorld.js
+++ src/plugin/starWorld.js
@@ -1,9 +1,8 @@
 import * as THREE from 'three'
-
 export const initial = (canvasDOM) => {
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    
+
   const renderer = new THREE.WebGLRenderer({
     canvas: canvasDOM
   })
@@ -16,14 +15,42 @@ export const initial = (canvasDOM) => {
     camera.updateProjectionMatrix()
   })
 
+  const balls = createBalls(scene)
+
   function animate () {
     requestAnimationFrame(animate)
+
+    camera.rotation.y -= 0.002
+    balls.forEach(ball => {
+      ball.rotation.x += 0.1
+      ball.rotation.y += 0.1
+    })
     renderer.render(scene, camera)
   }
+
+  camera.position.z = 1
 
   return {
     scene,
     camera,
     animate
   }
+}
+
+const createBalls = (scene) => {
+  const geometry = new THREE.SphereGeometry(0.5, 110, 140)
+  const material = new THREE.MeshNormalMaterial()
+
+  const balls = []
+  for (let index = 0; index < 1000; index++) {
+    const sphere = new THREE.Mesh(geometry, material)
+    const length = 50 + Math.random() * 10
+    sphere.position.x = length * Math.cos(Math.random() * 360)
+    sphere.position.y = length * Math.sin(Math.random() * 360)
+    sphere.position.z = length * Math.tan(Math.random() * 360)
+    scene.add(sphere)
+    balls.push(sphere)
+  }
+
+  return balls
 }
